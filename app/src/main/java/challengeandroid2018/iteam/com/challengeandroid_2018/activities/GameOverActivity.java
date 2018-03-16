813550aab98eefcc35af0f80467bf8634c2e28b3
Marianna-dl <marianna95220@gmail.com>
2018-03-16 10:48:00 +0100

    Add game over view

--- app/src/main/java/challengeandroid2018/iteam/com/challengeandroid_2018/activities/GameOverActivity.java
+++ app/src/main/java/challengeandroid2018/iteam/com/challengeandroid_2018/activities/GameOverActivity.java
@@ -1,15 +1,39 @@
 package challengeandroid2018.iteam.com.challengeandroid_2018.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import challengeandroid2018.iteam.com.challengeandroid_2018.R;
+import challengeandroid2018.iteam.com.challengeandroid_2018.util.Constant;
 
 public class GameOverActivity extends AppCompatActivity {
+
+    private TextView scoreTxt;
+    private Button backMenuBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        //Retrieve the timetable link to display
+        Intent i = getIntent();
+        String score = i.getStringExtra(Constant.INTENT_KEY_PLAYER_SCORE);
+
+        this.scoreTxt = findViewById(R.id.scoreTxt);
+        this.scoreTxt.append(score);
+
+        this.backMenuBtn = findViewById(R.id.backMenuBtn);
+        this.backMenuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(GameOverActivity.this, MenuActivity.class));
+            }
+        });
     }
+    
 }

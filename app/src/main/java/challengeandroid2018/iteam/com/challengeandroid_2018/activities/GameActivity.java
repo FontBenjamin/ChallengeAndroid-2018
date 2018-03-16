b263f89e530330b74d0ab9328ad8b99c7e9d5fe8
FontBenjamin <font.benjamin@yahoo.fr>
2018-03-16 10:20:27 +0100

    Jumping enabled

--- app/src/main/java/challengeandroid2018/iteam/com/challengeandroid_2018/activities/GameActivity.java
+++ app/src/main/java/challengeandroid2018/iteam/com/challengeandroid_2018/activities/GameActivity.java
@@ -1,15 +1,26 @@
 package challengeandroid2018.iteam.com.challengeandroid_2018.activities;
 
+import android.content.Context;
 import android.graphics.Rect;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import challengeandroid2018.iteam.com.challengeandroid_2018.R;
 
 public class GameActivity extends AppCompatActivity {
+
+    private ImageView imageViewCharacter;
+
+    private ConstraintLayout constraintLayoutGameActivity;
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,16 @@ public class GameActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_game);
 
+        this.context = this;
+        // we get the graphical elements
+        this.imageViewCharacter = (ImageView) findViewById(R.id.imageViewCharacter);
+        this.constraintLayoutGameActivity = findViewById(R.id.constraintLayoutGameActivity);
+        constraintLayoutGameActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateCharacterJump();
+            }
+        });
     }
 
     /**
@@ -32,5 +53,30 @@ public class GameActivity extends AppCompatActivity {
         Rect R1=new Rect(v1.getLeft(), v1.getTop(), v1.getRight(), v1.getBottom());
         Rect R2=new Rect(v2.getLeft(), v2.getTop(), v2.getRight(), v2.getBottom());
         return R1.intersect(R2);
+    }
+
+    /**
+     * make a jump animation on the view
+     */
+    public void animateCharacterJump(){
+        Animation jumpAnimation = AnimationUtils.loadAnimation(context, R.anim.jump_up);
+        jumpAnimation.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Animation hyperspaceJumpAnimation = AnimationUtils.loadAnimation(context, R.anim.jump_down);
+                imageViewCharacter.startAnimation(hyperspaceJumpAnimation);
+            }
+        });
+        this.imageViewCharacter.startAnimation(jumpAnimation);
+
     }
 }

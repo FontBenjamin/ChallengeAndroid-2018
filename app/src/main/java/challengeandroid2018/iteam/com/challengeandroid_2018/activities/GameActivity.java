631ee01f6c5a28b55843af62ee181632f13994e0
FontBenjamin <font.benjamin@yahoo.fr>
2018-03-16 09:43:10 +0100

    Added floor

--- app/src/main/java/challengeandroid2018/iteam/com/challengeandroid_2018/activities/GameActivity.java
+++ app/src/main/java/challengeandroid2018/iteam/com/challengeandroid_2018/activities/GameActivity.java
@@ -1,7 +1,11 @@
 package challengeandroid2018.iteam.com.challengeandroid_2018.activities;
 
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import challengeandroid2018.iteam.com.challengeandroid_2018.R;
 
@@ -10,6 +14,23 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_game);
+
+    }
+
+    /**
+     * this method handle the collision between two views
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public boolean CheckCollision(View v1, View v2) {
+        Rect R1=new Rect(v1.getLeft(), v1.getTop(), v1.getRight(), v1.getBottom());
+        Rect R2=new Rect(v2.getLeft(), v2.getTop(), v2.getRight(), v2.getBottom());
+        return R1.intersect(R2);
     }
 }

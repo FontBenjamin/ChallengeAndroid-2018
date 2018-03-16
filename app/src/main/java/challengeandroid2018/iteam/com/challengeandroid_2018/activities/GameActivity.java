cf389d3eb93ca2c7c6bc76a270984abf13088043
Marianna-dl <marianna95220@gmail.com>
2018-03-16 11:18:18 +0100

    End game redirect to game over

--- app/src/main/java/challengeandroid2018/iteam/com/challengeandroid_2018/activities/GameActivity.java
+++ app/src/main/java/challengeandroid2018/iteam/com/challengeandroid_2018/activities/GameActivity.java
@@ -1,5 +1,6 @@
 package challengeandroid2018.iteam.com.challengeandroid_2018.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,9 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    private void gameOver(){
+        startActivity(new Intent(GameActivity.this, GameOverActivity.class));
     }
 }

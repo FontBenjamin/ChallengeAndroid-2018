4ec0c79fcbed0929932c57793d8bda2ecd29dcb7
Marianna-dl <marianna95220@gmail.com>
2018-03-16 10:18:01 +0100

    Add game mode choice

---
+++ app/src/main/java/challengeandroid2018/iteam/com/challengeandroid_2018/Util/Constant.java
@@ -0,0 +1,11 @@
+package challengeandroid2018.iteam.com.challengeandroid_2018.Util;
+
+/**
+ * Created by Marianna on 16/03/2018.
+ */
+
+public class Constant {
+    public static final String INTENT_KEY_GAME_MODE = "mode";
+
+
+}

c1cfeac20f191899e92cc843535c9d9a8b519838
FontBenjamin <font.benjamin@yahoo.fr>
2018-03-16 09:08:01 +0100

    Added activities and packages

---
+++ app/src/main/java/challengeandroid2018/iteam/com/challengeandroid_2018/activities/GameOverActivity.java
@@ -0,0 +1,15 @@
+package challengeandroid2018.iteam.com.challengeandroid_2018.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import challengeandroid2018.iteam.com.challengeandroid_2018.R;
+
+public class GameOverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+    }
+}
